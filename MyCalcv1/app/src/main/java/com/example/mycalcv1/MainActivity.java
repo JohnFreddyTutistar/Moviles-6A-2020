5d0f76732669d642f7b5eb924cf6843bd4c03b1c
JohnFreddyTutistar <johnfre.157@gmail.com>
2020-11-03 22:31:44 -0500

    message

---
+++ MyCalcv1/app/src/main/java/com/example/mycalcv1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.mycalcv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //1. Create variables
+    private EditText n1;
+    private EditText n2;
+    private TextView ans;
+    private Button add;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //2. call components id´s (GUI) into variables
+        n1 = findViewById(R.id.et_n1);
+        n2 = findViewById(R.id.et_n2);
+        ans = findViewById(R.id.tv_ans);
+    }
+    public void Calc_Add(View view){
+        //3. Get values
+        String val1 = n1.getText().toString();
+        String val2 = n2.getText().toString();
+
+        int num1 = Integer.parseInt(val1);
+        int num2 = Integer.parseInt(val2);
+
+        int oper = num1 + num2;
+
+        String res = String.valueOf(oper);
+
+        ans.setText(res);
+
+    }
+    }
+}
