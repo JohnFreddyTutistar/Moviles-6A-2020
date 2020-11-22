bde82b72646feaa0e176dcce7e87e1133b404088
JohnFreddyTutistar <johnfre.157@gmail.com>
2020-11-21 20:25:56 -0500

    message

--- PalleteColors/app/src/main/java/com/example/palletecolors/MainActivity.java
+++ PalleteColors/app/src/main/java/com/example/palletecolors/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.palletecolors;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Show options menu
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu1, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Actions options menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+
+            case R.id.iteAboutOf :
+                Toast.makeText(this, "You've pressed About of option", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.iteHelp :
+                Toast.makeText(this, "You've pressed Help option", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.iteExit :
+                Toast.makeText(this, "You've pressed E xit option", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
