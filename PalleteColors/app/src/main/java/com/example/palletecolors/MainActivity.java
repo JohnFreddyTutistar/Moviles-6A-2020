f68fb87d276c3c4fad680a4cb6a732446527d808
JohnFreddyTutistar <johnfre.157@gmail.com>
2020-11-22 19:34:57 -0500

    message

--- PalleteColors/app/src/main/java/com/example/palletecolors/MainActivity.java
+++ PalleteColors/app/src/main/java/com/example/palletecolors/MainActivity.java
@@ -7,14 +7,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+
+    private SeekBar sbr_Red = null;
+    private SeekBar sbr_Green = null;
+    private SeekBar sbr_Blue = null;
+    private SeekBar sbr_Alpha = null;
+    private SeekBar view_Colors = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sbr_Red = findViewById(R.id.sbrRed);
+        sbr_Green = findViewById(R.id.sbrGreen);
+        sbr_Blue = findViewById(R.id.sbrBlue);
+        sbr_Alpha = findViewById(R.id.sbrAlpha);
+        //view_Colors = findViewById(R.id.viewColors);
     }
 
     //Show options menu
@@ -29,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
 
+            case R.id.iteSearch:
+                Toast.makeText(this, "You've pressed Search of option", Toast.LENGTH_SHORT).show()
+                break;
+
             case R.id.iteAboutOf :
                 Toast.makeText(this, "You've pressed About of option", Toast.LENGTH_SHORT).show();
                 break;
@@ -42,5 +60,20 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
